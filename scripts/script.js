f3363367c13ce102a9b19cc0119f4b561c47fb9c
juan daniel oter <jotadaniel2003@gmail.com>
2023-05-29 01:56:51 +0200

    comento el codigo de script.js

--- scripts/script.js
+++ scripts/script.js
@@ -5,32 +5,37 @@
         const searchButton = document.querySelector(".search button");
         const weatherIcon=document.querySelector(".weather-icon"); 
 
-        async function getWeatherData(city){
-            const response = await fetch(apiUrl+city+ `&appid=${apiKey}`);
+        async function getWeatherData(city) {
+            // Realiza una solicitud a la API de OpenWeatherMap con la ciudad proporcionada y la apiKey.
+            const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+            
+            // Convierte la respuesta en formato JSON en un objeto JavaScript.
             var data = await response.json();
-
-
+          
+            // Actualiza los elementos del DOM con la información del clima obtenida.
             document.querySelector(".city").innerHTML = data.name;
             document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
             document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
             document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-
-            if(data.weather[0].main == "Clear"){
-                weatherIcon.src = "clear.png";
-            }else if(data.weather[0].main == "Clouds"){ 
-                weatherIcon.src = "clouds.png";
-            }else if(data.weather[0].main == "Rain"){   
-                weatherIcon.src = "rain.png";
-            }else if(data.weather[0].main == "Mist"){
-                weatherIcon.src.src = "mist.png";
-            }else if(data.weather[0].main == "Drizzle"){
-                weatherIcon.src.src = "drizzle.png"; 
-            }else if(data.weather[0].main == "Snow"){
-                weatherIcon.src = "snow.png";
+          
+            // Establece la imagen del ícono del clima según el valor de data.weather[0].main.
+            if (data.weather[0].main == "Clear") {
+              weatherIcon.src = "clear.png";
+            } else if (data.weather[0].main == "Clouds") {
+              weatherIcon.src = "clouds.png";
+            } else if (data.weather[0].main == "Rain") {
+              weatherIcon.src = "rain.png";
+            } else if (data.weather[0].main == "Mist") {
+              weatherIcon.src = "mist.png"; 
+            } else if (data.weather[0].main == "Drizzle") {
+              weatherIcon.src = "drizzle.png"; 
+            } else if (data.weather[0].main == "Snow") {
+              weatherIcon.src = "snow.png";
             }
-
-            document.querySelector(".weather").style.display="block";
-        }        
+          
+            // Muestra el elemento con clase "weather" estableciendo su propiedad display en "block".
+            document.querySelector(".weather").style.display = "block";
+          }   
         searchButton.addEventListener("click", ()=>{
             getWeatherData(searchBox.value);
         });
